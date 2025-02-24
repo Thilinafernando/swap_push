7138a2fdd4ab3b2888cbacd59e379bc2ca50b79b
thilinafernando <thilinaetoro4575@gmail.com>
2025-02-24 20:55:33 +0100

    again

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,47 +6,48 @@
 /*   By: tkurukul <thilinaetoro4575@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:32:08 by tkurukul          #+#    #+#             */
-/*   Updated: 2025/02/24 19:41:36 by tkurukul         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:52:04 by tkurukul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../source/push_swap.h"
 
-int	operations(t_swap *a, t_swap *b, char *str)
+int	operations(t_swap **a, t_swap **b, char *str)
 {
 	if (ft_strcmp(str, "sa\n") == 0)
-		sa(a, 0);
+		sa(*a, 0);
 	else if (ft_strcmp(str, "sb\n") == 0)
-		sb(b, 0);
+		sb(*b, 0);
 	else if (ft_strcmp(str, "ss\n") == 0)
-		ss(a, b, 0);
+		ss(*a, *b, 0);
 	else if (ft_strcmp(str, "pa\n") == 0)
-		pa(&a, &b, 0);
+		pa(a, b, 0);
 	else if (ft_strcmp(str, "pb\n") == 0)
-		pb(&a, &b, 0);
+		pb(a, b, 0);
 	else if (ft_strcmp(str, "ra\n") == 0)
-		ra(&a, 0);
+		ra(a, 0);
 	else if (ft_strcmp(str, "rb\n") == 0)
-		rb(&b, 0);
+		rb(b, 0);
 	else if (ft_strcmp(str, "rr\n") == 0)
-		rr(&a, &b, 0);
+		rr(a, b, 0);
 	else if (ft_strcmp(str, "rra\n") == 0)
-		rra(&a, 0);
+		rra(a, 0);
 	else if (ft_strcmp(str, "rrb\n") == 0)
-		rrb(&b, 0);
+		rrb(b, 0);
 	else if (ft_strcmp(str, "rrr\n") == 0)
-		rrr(&a, &b, 0);
+		rrr(a, b, 0);
 	else
 		return (write(2, "ERROR\n", 6), 0);
 	return (1);
 }
 
-void	last(t_swap *a)
+int	last(t_swap *a, int i)
 {
 	if (a && is_sorted(&a) == 1)
-		ft_printf("OK\n");
-	else
-		ft_printf("KO\n");
+		return (ft_printf("OK\n"), 1);
+	else if (is_sorted(&a) == 0 && i == 1)
+		return (ft_printf("KO\n"), 0);
+	return (0);
 }
 
 int	main(int ac, char **av)
@@ -65,15 +66,15 @@ int	main(int ac, char **av)
 	a = lstcreation(matrix);
 	if (!a)
 		return (0);
+	if (last(a, 0) == 1)
+		return(free_list(a), 0);
 	str = get_next_line(0);
 	while (str)
 	{
-		if (operations(a, b, str) == 0)
+		if (operations(&a, &b, str) == 0)
 			return (free_list(a), free_list(b), free(str), 0);
 		free(str);
 		str = get_next_line(0);
 	}
-	free(str);
-	last(a);
-	return (free_list(a), free_list(b), 0);
+	return (last(a, 1), free_list(a), free_list(b), free(str), 0);
 }
